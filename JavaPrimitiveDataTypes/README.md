9b2dd7b3c3af6b52becbe5990e3b246bd9d7fe58
Scalro <rotichcaleb200@gmail.com>
2023-05-05 09:33:07 +0300

    Primitive Data Types

---
+++ JavaPrimitiveDataTypes/README.md
@@ -0,0 +1,1 @@
+These are Primitive Data Types
---
+++ JavaPrimitiveDataTypes/src/primitiveDataTypes/PrimitiveDataTypes.java
@@ -0,0 +1,23 @@
+package primitiveDataTypes;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		int num = 350000;
+		char letter = 'A';
+		byte n = 8;
+		float dec = 234.23456f;
+		double doub = 3456.456784567d;
+		boolean check = true;
+		long longInt = 234567899;
+		
+		System.out.println("Integer: " + num);
+		System.out.println("Character: " + letter);
+		System.out.println("Byte: " + n);
+		System.out.println("Float: " + dec);
+		System.out.println("Double: " + doub);
+		System.out.println("Boolean: " + check);
+		System.out.println("Long Integer: " + longInt);
+	}
+
+}
