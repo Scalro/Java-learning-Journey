8bd66c42c751bf5325ed056367466dfd4ea97111
Scalro <rotichcaleb200@gmail.com>
2023-05-05 16:09:01 +0300

    Arrays in Java

---
+++ ArraysInJava/src/arraysInJava/ArraysInJava.java
@@ -0,0 +1,35 @@
+package arraysInJava;
+
+public class ArraysInJava {
+
+	public static void main(String[] args) {
+		
+		int[] arR;
+		arR = new int [10];
+		
+		arR[0] = 100;
+		arR[1] = 101;
+		arR[2] = 102;
+		arR[3] = 103;
+		arR[4] = 104;
+		arR[5] = 105;
+		arR[6] = 106;
+		arR[7] = 107;
+		arR[8] = 108;
+		arR[9] = 109;
+		
+		
+		System.out.println("The element a index 0 is : " + arR[0]);
+		System.out.println("The element a index 1 is : " + arR[1]);
+		System.out.println("The element a index 2 is : " + arR[2]);
+		System.out.println("The element a index 3 is : " + arR[3]);
+		System.out.println("The element a index 4 is : " + arR[4]);
+		System.out.println("The element a index 5 is : " + arR[5]);
+		System.out.println("The element a index 6 is : " + arR[6]);
+		System.out.println("The element a index 7 is : " + arR[7]);
+		System.out.println("The element a index 8 is : " + arR[8]);
+		System.out.println("The element a index 9 is : " + arR[9]);
+
+	}
+
+}
