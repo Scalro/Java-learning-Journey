17dbaf90b42cebdaf4c8bd54270050a26f402355
Scalro <rotichcaleb200@gmail.com>
2023-05-08 09:31:56 +0300

    Comparison Operators

---
+++ ArithmetricOperatorsInJava/src/comparisonOperators/ComparisonOperators.java
@@ -0,0 +1,31 @@
+package comparisonOperators;
+
+public class ComparisonOperators {
+
+	public static void main(String[] args) {
+		
+		int val1 = 5;
+		int val2 = 7;
+		
+		
+		if (val1 == val2)
+			System.out.println("val1 == val2");
+		
+		if (val1 < val2)
+			System.out.println("val1 < val2");
+		
+		if (val1 > val2)
+			System.out.println("val1 > val2");
+		
+		if (val1 <= val2)
+			System.out.println("val1 <= val2");
+		
+		if (val1 >= val2)
+			System.out.println("val1 >= val2");
+		
+		if (val1 != val2)
+			System.out.println("val1 != val2");
+
+	}
+
+}
