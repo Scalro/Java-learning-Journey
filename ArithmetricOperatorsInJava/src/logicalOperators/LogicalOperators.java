1493e970e25775ef3e0c8589ec0eed97083bdd69
Scalro <rotichcaleb200@gmail.com>
2023-05-08 09:40:20 +0300

    Logical operators

---
+++ ArithmetricOperatorsInJava/src/logicalOperators/LogicalOperators.java
@@ -0,0 +1,18 @@
+package logicalOperators;
+
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+		
+		int val1 = 9;
+		int val2 = 11;
+		
+		if ((val1 == 1) && (val2 == 11))
+			System.out.println("val1 == 9 AND val2 == 11");
+		
+		if ((val1 == 9) || (val2 == 10))
+			System.out.println("val1 == 9 OR val2 == 11");
+
+	}
+
+}
